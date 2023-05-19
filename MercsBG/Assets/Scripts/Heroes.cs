d146cb650f8536c541026029e0f46da50dbdb1da
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-20 06:18:41 +0800

    Player

---
+++ MercsBG/Assets/Scripts/Heroes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroes : MonoBehaviour, IHeroes
+{
+   #region VARIABLES
+   
+   [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IHero))] private List<Object> playerHeroes = new List<Object>();
+   
+
+
+   #endregion
+        
+   #region PROPERTIES
+
+   public List<IHero> PlayerHeroes
+   {
+      get
+      {
+         var newHeroes = new List<IHero>();
+         foreach (var playerHero in playerHeroes)
+         {
+            newHeroes.Add(playerHero as IHero);
+         }
+         return newHeroes;
+      }
+   }
+
+   public List<IHero> AliveHeroes { get; set; } = new List<IHero>();
+   
+   public List<IHero> DeadHeroes { get; set; } = new List<IHero>();
+   
+   public List<IHero> ExtinctHeroes { get; set; } = new List<IHero>();
+
+
+   #endregion
+        
+   #region METHODS
+
+        
+
+   #endregion
+}
---
+++ MercsBG/Assets/Scripts/IHeroes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public interface IHeroes
+{
+    List<IHero> PlayerHeroes { get; }
+    List<IHero> AliveHeroes { get; set; }
+    List<IHero> DeadHeroes { get; set; }
+    List<IHero> ExtinctHeroes { get; set; }
+}
