4183bf2d5c29b0a2edb58107aa35b0a3f2e4b3c3
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-20 05:26:55 +0800

    StatusEffects WIP

---
+++ MercsBG/Assets/Scripts/IStatusEffect.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public interface IStatusEffect
+{
+    Canvas Canvas { get; }
+    void SetFrame(Sprite newFrame);
+    void SetIcon(Sprite newIcon);
+    void SetCounters(int newCounters);
+}
--- MercsBG/Assets/Scripts/StatusEffect.cs
+++ MercsBG/Assets/Scripts/StatusEffect.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StatusEffect : MonoBehaviour
+public class StatusEffect : MonoBehaviour, IStatusEffect
 {
    #region VARIABLES
 
