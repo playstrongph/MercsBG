4a701fe8e6f3e70d2f5a7da908d841da43b568cd
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-19 14:04:11 +0800

    Hero WIP

---
+++ MercsBG/Assets/Scripts/HeroMasks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroMasks : MonoBehaviour
+{
+   #region VARIABLES
+
+   [SerializeField] private Image maskImage = null;
+
+   [SerializeField] private List<Sprite> maskSprites = new List<Sprite>();
+
+   #endregion
+
+   #region PROPERTIES
+
+   private List<Sprite> MaskSprites => maskSprites;
+   private Image MaskImage => maskImage;
+
+   #endregion
+
+   #region METHODS
+
+   public void SetRedMask()
+   {
+      MaskImage.sprite = MaskSprites[0];
+   }
+   
+   public void SetGreenMask()
+   {
+      MaskImage.sprite = MaskSprites[1];
+   }
+   
+   public void SetBlueMask()
+   {
+      MaskImage.sprite = MaskSprites[2];
+   }
+
+   #endregion
+}
