5db3b6ea71f41e7b18f4b2dca5660c6e85d690bb
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-19 12:46:48 +0800

    Hero Frames

--- MercsBG/Assets/Scripts/HeroFrames.cs
+++ MercsBG/Assets/Scripts/HeroFrames.cs
@@ -1,24 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HeroFrames : MonoBehaviour
+public class HeroFrames : MonoBehaviour, IHeroFrames
 {
    #region VARIABLES
 
-        
+   [SerializeField] private List<Sprite> frames = new List<Sprite>();
+
 
    #endregion
-        
+
    #region PROPERTIES
-
-        
+   private List<Sprite> Frames => frames;
 
    #endregion
-        
-   #region METHODS
 
-        
+   #region METHODS
+   
+   public void SetColorRedDefender(Image image)
+   {
+      //Element 0 is Red
+      image.sprite = Frames[0];
+   }
+   
+   public void SetColorGreenFighter(Image image)
+   {
+      //Element 1 is Green
+      image.sprite = Frames[1];
+   }
+   
+   public void SetColorBlueCaster(Image image)
+   {
+      //Element 2 is Blue
+      image.sprite = Frames[2];
+   }
+   
+   
+
+
 
    #endregion
 }
---
+++ MercsBG/Assets/Scripts/IHeroFrames.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.UI;
+
+public interface IHeroFrames
+{
+    void SetColorRedDefender(Image image);
+    void SetColorGreenFighter(Image image);
+    void SetColorBlueCaster(Image image);
+}
