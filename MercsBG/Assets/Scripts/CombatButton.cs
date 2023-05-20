6ee9572fe6ada69ef7f961f80072045050acf472
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-21 06:06:51 +0800

    Button WIP

---
+++ MercsBG/Assets/Scripts/CombatButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatButton : MonoBehaviour, ICombatButton
+{
+   #region VARIABLES
+
+   [SerializeField] private Image buttonImage = null;
+   [SerializeField] private List<Sprite> buttonSprites = new List<Sprite>();
+   [SerializeField] private TextMeshProUGUI buttonText = null;
+
+
+   #endregion
+
+   #region PROPERTIES
+
+   private Image ButtonImage => buttonImage;
+   
+   /// <summary>
+   /// Sequence: Grey - Green - Yellow
+   /// </summary>
+   private List<Sprite> ButtonSprites => buttonSprites;
+
+   private TextMeshProUGUI ButtonText => buttonText;
+
+
+   #endregion
+
+   #region METHODS
+
+   public void SetButtonText(string text)
+   {
+      ButtonText.text = text;
+   }
+
+   public void SetGreyButton()
+   {
+      ButtonImage.sprite = ButtonSprites[0];
+   }
+   
+   public void SetGreenButton()
+   {
+      ButtonImage.sprite = ButtonSprites[1];
+   }
+   
+   public void SetYellowButton()
+   {
+      ButtonImage.sprite = ButtonSprites[2];
+   }
+   
+   
+
+
+
+
+   #endregion
+}
---
+++ MercsBG/Assets/Scripts/ICombatButton.cs
@@ -0,0 +1,7 @@
+﻿public interface ICombatButton
+{
+    void SetButtonText(string text);
+    void SetGreyButton();
+    void SetGreenButton();
+    void SetYellowButton();
+}
