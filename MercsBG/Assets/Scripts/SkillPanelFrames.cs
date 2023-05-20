a3fa2d8fba7dd26233a1fbd23ddccadcf7415919
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-20 10:23:55 +0800

    Skill Panel

---
+++ MercsBG/Assets/Scripts/SkillPanelFrames.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPanelFrames : MonoBehaviour
+{
+   #region VARIABLES
+
+        
+
+   #endregion
+        
+   #region PROPERTIES
+
+        
+
+   #endregion
+        
+   #region METHODS
+
+        
+
+   #endregion
+}
