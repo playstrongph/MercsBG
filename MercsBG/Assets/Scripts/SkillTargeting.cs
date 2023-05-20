21229ee0d5b286f80047e7768a131b8ec2718edd
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-21 05:45:16 +0800

    Skill Targeting

---
+++ MercsBG/Assets/Scripts/SkillTargeting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTargeting : MonoBehaviour
+{
+   #region VARIABLES
+
+   [SerializeField] private GameObject arrow = null;
+   [SerializeField] private GameObject crossHair = null;
+   [SerializeField] private List<GameObject> nodes = new List<GameObject>();
+
+   #endregion
+
+   #region PROPERTIES
+
+   private GameObject Arrow => arrow;
+   private GameObject CrossHair => crossHair;
+   private List<GameObject> Nodes => nodes;
+
+   #endregion
+
+   #region METHODS
+
+
+
+   #endregion
+}
