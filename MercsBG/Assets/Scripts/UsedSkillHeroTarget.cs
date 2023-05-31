e0cd4bd2e988ce2942fdfb004e4a362e5596dfd2
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-31 13:48:32 +0800

    Used Skill Preview

---
+++ MercsBG/Assets/Scripts/UsedSkillHeroTarget.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+public class UsedSkillHeroTarget : MonoBehaviour
+{
+   #region VARIABLES
+
+   [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IHeroFrames))] private Object heroFrames = null;
+   
+   [Header("Used Skill Images")]
+   [SerializeField] private Image heroFrame = null;
+   [SerializeField] private Image armorImage = null;
+   [SerializeField] private Image damageImage = null;
+   [SerializeField] private Image criticalDamageImage = null;
+
+   [Header("Used Skill Texts")] 
+   [SerializeField] private TextMeshProUGUI attackText = null;
+   [SerializeField] private TextMeshProUGUI healthText = null;
+   [SerializeField] private TextMeshProUGUI armorText = null;
+   [SerializeField] private TextMeshProUGUI nameText = null;
+   [SerializeField] private TextMeshProUGUI raceText = null;
+   [SerializeField] private TextMeshProUGUI damageText = null;
+   
+
+
+   #endregion
+
+   #region PROPERTIES
+   
+   public IHeroFrames HeroFrames => heroFrames as IHeroFrames;
+
+   public Image HeroFrame => heroFrame;
+   public Image ArmorImage => armorImage;
+   public Image DamageImage => damageImage;
+   public Image CriticalDamageImage => criticalDamageImage;
+
+   public TextMeshProUGUI AttackText => attackText;
+   public TextMeshProUGUI HealthText => healthText;
+   public TextMeshProUGUI ArmorText => armorText;
+   public TextMeshProUGUI NameText => nameText;
+   public TextMeshProUGUI RaceText => raceText;
+   public TextMeshProUGUI DamageText => damageText;
+
+   #endregion
+
+   #region METHODS
+
+   private void Awake()
+   {
+      
+   }
+
+   #endregion
+}
