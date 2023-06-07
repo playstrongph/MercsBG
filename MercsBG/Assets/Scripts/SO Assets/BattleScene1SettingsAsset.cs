e2342af703a616f216c852a26e03cc0c5eb74cfe
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-06-08 06:54:49 +0800

    BattleSceneManager

--- MercsBG/Assets/Scripts/SO Assets/BattleScene1SettingsAsset.cs
+++ MercsBG/Assets/Scripts/SO Assets/BattleScene1SettingsAsset.cs
@@ -8,16 +8,21 @@ namespace SO_Assets
    {
       #region VARIABLES
 
-      [Header("SCRIPTABLE OBJECT ASSETS")]
+      [Header("TEAM HEROES")]
       [SerializeField]  [RequireInterfaceAttribute.RequireInterface(typeof(ITeamHeroesAsset))] private ScriptableObject allyTeamHeroes; 
-      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ITeamHeroesAsset))]private ScriptableObject enemyTeamHeroes;  
+      [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ITeamHeroesAsset))]private ScriptableObject enemyTeamHeroes;
+
+      [Header("GAME BOARD IMAGE")] 
+      [SerializeField] private Sprite boardImage = null;
 
       #endregion
         
       #region PROPERTIES
 
       public ITeamHeroesAsset AllyTeamHeroes { get => allyTeamHeroes as ITeamHeroesAsset; private set => allyTeamHeroes = value as ScriptableObject; }
-      public ITeamHeroesAsset EnemyTeamHeroes { get => enemyTeamHeroes as ITeamHeroesAsset; private set => enemyTeamHeroes = value as ScriptableObject; }  
+      public ITeamHeroesAsset EnemyTeamHeroes { get => enemyTeamHeroes as ITeamHeroesAsset; private set => enemyTeamHeroes = value as ScriptableObject; }
+
+      public Sprite BoardImage => boardImage;
       
       
       #endregion
--- MercsBG/Assets/Scripts/SO Assets/IBattleScene1SettingsAsset.cs
+++ MercsBG/Assets/Scripts/SO Assets/IBattleScene1SettingsAsset.cs
@@ -1,8 +1,11 @@
-﻿namespace SO_Assets
+﻿using UnityEngine;
+
+namespace SO_Assets
 {
     public interface IBattleScene1SettingsAsset
     {
         ITeamHeroesAsset AllyTeamHeroes { get; }
         ITeamHeroesAsset EnemyTeamHeroes { get; }
+        Sprite BoardImage { get; }
     }
 }
