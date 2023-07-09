d64093e3597467c08eb906b501e65da84dbe4191
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-07-09 12:44:58 +0800

    Update ShowLineAndCrossHair.cs

--- MercsBG/Assets/Scripts/ShowLineAndCrossHair.cs
+++ MercsBG/Assets/Scripts/ShowLineAndCrossHair.cs
@@ -45,6 +45,7 @@ public class ShowLineAndCrossHair : MonoBehaviour, IShowLineAndCrossHair
          ShowArrow(notNormalized,direction);
             
          //SkillTargetCollider.TargetNodes.ShowArrowNodes();
+         ShowArrowNodes();
             
          //ShowTargetCrossHair();
          
@@ -61,6 +62,11 @@ public class ShowLineAndCrossHair : MonoBehaviour, IShowLineAndCrossHair
       for (int i = 0; i < 3; i++)
       {
          _controlPoints.Add(Vector3.zero);
+      }
+
+      foreach (var node in SkillTargeting.SkillTargetingCollider.Nodes)
+      {
+         _arrowNodes.Add(node);
       }
    }
    
@@ -79,6 +85,56 @@ public class ShowLineAndCrossHair : MonoBehaviour, IShowLineAndCrossHair
       arrow.gameObject.transform.rotation = Quaternion.Euler(0f,0f,rotZ-90);
    }
    
+       
+   private void ShowArrowNodes()
+   {
+      //Transform where the mouse currently is
+      var mouseTransform = transform;
+
+      //Transform where the skill parent currently is
+      var skillTransform = SkillTargeting;
+           
+      //P0 is where the mouse is at
+      _controlPoints[0] = mouseTransform.position;
+           
+      //P2 is where the source (skill parent) is at
+      _controlPoints[2] = SkillTargeting.Transform.position;
+
+      //Halfway between P0 and P2, with a height of Z
+      _controlPoints[1] =
+         _controlPoints[0] + ((_controlPoints[2] - _controlPoints[0])/2) + new Vector3(0, 0, 100);
+
+
+      for (var i = 0; i < _arrowNodes.Count; i++)
+      {
+               
+         //Don't ask.  Haha!
+         var t = (i+1.5f) / ((_arrowNodes.Count - 1) + 1f);
+
+         //Quadratic Bezier Curve
+         _arrowNodes[i].transform.position =
+            Mathf.Pow(1 - t, 2) * _controlPoints[0] +           //(1-t)^2*P0
+            2 * Mathf.Pow(1 - t, 1) * t * _controlPoints[1] +   //2*(1-t)*t*p1
+            Mathf.Pow(t, 2) * _controlPoints[2];                //t^2*P2
+               
+               
+         //Calculates rotation for each arrow node
+         if (i>0)
+         {
+            var euler = new Vector3(0, 0, 
+               Vector2.SignedAngle(Vector2.up, _arrowNodes[i].transform.position - _arrowNodes[i - 1].transform.position));
+            _arrowNodes[i].transform.rotation = Quaternion.Euler(euler);
+         }
+   
+         //calculates scales for each arrow node
+         var scale = scaleFactor * (1f - 0.03f * (_arrowNodes.Count - 1 - i));
+               
+         _arrowNodes[i].transform.localScale = new Vector3(scale, scale, 1f);
+
+         //_arrowNodes[i].GetComponent<Image>().enabled = true;
+               
+      }//End of for statement
+   }
     
    
    
